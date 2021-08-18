0ad3d0daffc22ecedd0adb3a26d53be7e34361b1
Justin Paulson <jpaulson@aha.io>
2021-08-18 12:52:37 -0500

    Initial commit: asana auth test"

---
+++ src/importers/asanaAuth.js
@@ -0,0 +1,44 @@
+const importer = aha.getImporter("aha-develop.asanaAuth");
+
+/**
+   * @param useCache force a full re-auth even if the token is already cached
+   */
+async function authenticate(useCache = true) {
+  const authData = await aha.auth("asana", {});
+  console.log("We got a token!", authData.token);
+}
+
+importer.on({ action: "listCandidates" }, async ({ filters, nextPage }, {identifier, settings}) => {
+  await authenticate();
+  return { records: [], nextPage: 2 };
+});
+
+//Optional
+//importer.on({ action: "listFilters" }, ({}, {identifier, settings}) => {
+//  return {
+//    filterName: {
+//      title: "Filter Name",
+//      required: true,
+//      type: "text",
+//    },
+//  };
+//});
+
+//Optional
+//importer.on({ action: "filterValues" }, async ({ filterName, filters }, {identifier, settings}) => {
+//  return [{ text: "Filter Text", value: "Filter Value" }];
+//});
+
+//Optional
+//importer.on({ action: "renderRecord" }, ({ record, onUnmounted }, { identifier, settings }) => {
+//  onUnmounted(() => {
+//    console.log("Un-mounting component for", record.identifier);
+//  });
+//
+//  return `${record.identifier} ${record.name}`;
+//});
+
+//Optional
+//importer.on({ action: "importRecord" }, async ({ importRecord, ahaRecord }, {identifier, settings}) => {
+//  //Import record code goes here
+//});
